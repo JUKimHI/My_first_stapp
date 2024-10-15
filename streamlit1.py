a7aca9e26a2944ae5398ee5abda216557d0596e0
JUKimHI <166390762+JUKimHI@users.noreply.github.com>
2024-10-15 11:59:43 +0900

    Update streamlit1.py

--- streamlit1.py
+++ streamlit1.py
@@ -62,3 +62,36 @@ st.sidebar.checkbox('체크박스 1')
 st.sidebar.checkbox('체크박스 2')
 st.sidebar.radio('라디오 버튼', ['radio 1', 'radio 2', 'radio 3'])
 st.sidebar.selectbox('셀렉트박스', ['select 1', 'select 2', 'select 3'])
+
+
+# 레이아웃: 컬럼
+st.header('🧑🏻‍🚀 컬럼 레이아웃')
+col_1, col_2, col_3 = st.columns([1,2,1]) # 컬럼 인스턴스 생성. 1:2:1 비율로 컬럼을 나눔
+
+with col_1:
+    st.write('## 1번 컬럼')
+    st.checkbox('이것은 1번 컬럼에 속한 체크박스 1')
+    st.checkbox('이것은 1번 컬럼에 속한 체크박스 2')
+
+with col_2:
+    st.write('## 2번 컬럼')
+    st.radio('2번 컬럼의 라디오 버튼', ['radio 1', 'radio 2', 'radio 3']) # 동일한 라디오 버튼을 생성할 수 없음
+    # 사이드바에 이미 라디오 버튼이 생성되어 있기 때문에, 여기서는 라디오 버튼의 내용을 변경해야 오류가 발생하지
+
+col_3.write('## 3번 컬럼')
+col_3.selectbox('3번 컬럼의 셀렉트박스', ['select 1', 'select 2', 'select 3'])
+# 사이드바에 이미 셀렉트박스가 생성되어 있기 때문에, 여기서는 셀렉트박스의 내용을 변경해야 오류가 발생하지 않음
+
+# 레이아웃: 탭
+st.header('👮🏻 탭 레이아웃')
+
+# 탭 인스턴스 생성. 3개의 탭을 생성
+tab_1, tab_2, tab_3 = st.tabs(['탭AAAAA', '탭BBBBB', '탭CCCCC'])
+with tab_1:
+    st.write('## 탭AAAAA')
+    st.write('이것은 탭A의 내용입니다.')
+with tab_2:
+    st.write('## 탭BBBBB')
+    st.write('이것은 탭B의 내용입니다.')
+tab_3.write('## 탭CCCCC')
+tab_3.write('이것은 탭C의 내용입니다.')
